40d61bcb08488a6f739615a67c8015c232fbaac1
Paulo <ph.fernands90@gmail.com>
2022-07-16 00:58:59 -0300

    consultas com parametros dinamicos e com Criteria API

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,9 +1,15 @@
 package br.com.alura.loja.dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.alura.loja.modelo.Produto;
 
@@ -48,6 +54,59 @@ public class ProdutoDAO {
 		return em.createQuery(jpql, BigDecimal.class)
 				.setParameter("nome", nome)
 				.getSingleResult();
+	}
+	
+	public List<Produto> buscarPorParametro(String nome, BigDecimal preco, LocalDate dataCadastro) {
+		
+		String jpql = "SELECT p FROM Produto p WHERE 1=1 ";
+		if(nome != null && !nome.trim().isEmpty()) {
+			jpql = " AND p.nome = :nome ";
+		}
+		if(preco != null) {
+			jpql = " AND p.preco = :preco ";
+		}
+		if(dataCadastro != null) {
+			jpql = " AND p.dataCadastro = :dataCadastro ";
+		}
+		
+		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+		
+		if(nome != null && !nome.trim().isEmpty()) {
+			query.setParameter("nome", nome);
+		}
+		if(preco != null) {
+			query.setParameter("preco", preco);
+		}
+		if(dataCadastro != null) {
+			query.setParameter("dataCadastro", dataCadastro);
+		}
+		
+		return query.getResultList();
+		
+	}
+	
+	public List<Produto> buscarPorParametroComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro) {
+		
+		CriteriaBuilder criteria = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = criteria.createQuery(Produto.class);
+		
+		Root<Produto> from = query.from(Produto.class);
+		
+		Predicate filtros = criteria.and();
+		
+		if(nome != null && !nome.trim().isEmpty()) {
+			filtros = criteria.and(filtros, criteria.equal(from.get("nome"), nome));
+		}
+		if(preco != null) {
+			filtros = criteria.and(filtros, criteria.equal(from.get("preco"), preco));
+		}
+		if(dataCadastro != null) {
+			filtros = criteria.and(filtros, criteria.equal(from.get("dataCadastro"), dataCadastro));
+		}
+		
+		query.where(filtros);
+		return em.createQuery(query).getResultList();
+		
 	}
 	
 	
---
+++ src/main/java/br/com/alura/loja/testes/TesteCriteria.java
@@ -0,0 +1,76 @@
+package br.com.alura.loja.testes;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ClienteDAO;
+import br.com.alura.loja.dao.PedidoDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Cliente;
+import br.com.alura.loja.modelo.ItemPedido;
+import br.com.alura.loja.modelo.Pedido;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+public class TesteCriteria {
+
+	public static void main(String[] args) {
+		
+		popularBancoDeDados();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO pDao = new ProdutoDAO(em);
+		List<Produto> p = pDao.buscarPorParametroComCriteria(null, null, LocalDate.now());
+		
+		p.forEach(produto -> System.out.println(produto.getDescricao() + " " + produto.getDataCadastro()));
+		
+	}
+	
+	private static void popularBancoDeDados() {
+		
+		Categoria monitor = new Categoria("Monitor");
+		Categoria celular = new Categoria("Celular");
+		Categoria periferico = new Categoria("Periférico");
+		
+        Produto p1 = new Produto("AOC Hero", "27 polegadas c/G-Sync", new BigDecimal("2000"), monitor );
+        Produto p2 = new Produto("Iphone 13", "Lançamento Apple", new BigDecimal("5000"), celular);
+        Produto p3 = new Produto("Mouse Razer", "Mouse gamer c/RGB", new BigDecimal("150"), periferico);
+        
+        Cliente cliente = new Cliente("Claudio", "587741");
+        Cliente cliente2 = new Cliente("Jessica", "885741");
+        
+        Pedido pedido = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(5, pedido, p1));
+        pedido.adicionarItem(new ItemPedido(10, pedido, p3));
+        
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDao = new ProdutoDAO(em);
+        CategoriaDAO categoriaDao = new CategoriaDAO(em);
+        ClienteDAO clienteDao = new ClienteDAO(em);
+        PedidoDAO pedidoDao = new PedidoDAO(em);
+ 
+        em.getTransaction().begin();
+
+        categoriaDao.cadastrar(monitor);
+        categoriaDao.cadastrar(periferico);
+        categoriaDao.cadastrar(celular);
+        
+        produtoDao.cadastrar(p1);
+        produtoDao.cadastrar(p2);
+        produtoDao.cadastrar(p3);
+        
+        clienteDao.cadastrar(cliente2);
+        clienteDao.cadastrar(cliente);
+        
+        pedidoDao.cadastrar(pedido);
+
+        em.getTransaction().commit();
+        em.close();
+        
+	}
+		
+}
